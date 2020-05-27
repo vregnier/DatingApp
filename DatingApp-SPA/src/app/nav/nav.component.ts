953366b427e8a51198c768b4c5edc98f63bbefd9
Vincent Regnier <vincent.regnier@e-xstream.com>
2020-05-27 21:35:16 +0200

    End of section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};  // object to store username and password
 
-  constructor(private authService: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
@@ -17,22 +18,21 @@ export class NavComponent implements OnInit {
   login(){
     this.authService.login(this.model).subscribe(
       next => {
-      console.log('Logged is success');
+      this.alertify.success('Logged is success');
       },
       error => {
-      console.log(error);
+      this.alertify.error(error);
       }
     );
   }
 
   loggedIn(){
-    const token = localStorage.getItem('token');
-    return !!token;  // if token empty return false if not return true
+    return this.authService.loggedIn();
   }
 
   logout(){
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +12,7 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private alertify: AlertifyService) {
     
    }
 
@@ -20,9 +21,9 @@ export class RegisterComponent implements OnInit {
 
   register(){
     this.authService.register(this.model).subscribe( () => {
-      console.log('registration successful');
+      this.alertify.success('registration successful');
     }, error => {          // lambda fct with argument error and just log this error
-      console.log(error);
+      this.alertify.error(error);
     });
     // we must subscribe because register method return an observable
   }
