6f28a5589f17a30aa6a587e09187d9ab341f96a3
Vincent Regnier <vincent.regnier@e-xstream.com>
2020-06-10 15:42:22 +0200

    before to change DB

---
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { User } from 'src/app/_models/user';
+import { ActivatedRoute } from '@angular/router';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css']
+})
+export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm',{static: true}) editForm: NgForm;
+  user: User;
+  @HostListener('window:beforeunload', ['$envent'])
+  unloadNotification($envent: any) {
+    if(this.editForm.dirty) {
+      $envent.returnValue = true;
+    }
+  }
+
+  constructor(
+    private route: ActivatedRoute,
+    private alertify: AlertifyService
+  ) { }
+
+  ngOnInit() {
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
+    });
+  }
+
+  updateUser() {
+    console.log(this.user);
+    this.alertify.success('Profile updated successfully');
+    this.editForm.reset(this.user);
+  }
+
+}
